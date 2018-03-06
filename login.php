3e5fed256b05375de7522a677733298be0b10061
hongdanh158 <lthdanh@gmail.com>
2018-03-06 22:04:26 +0700

    Update

---
+++ login.php
@@ -0,0 +1,79 @@
+<?php include('header.php') ?>
+    <div class="head-space"></div>
+    <!-- <div class="head-background">
+      <img src="images/ex/slider/slider-6.jpg" alt="">
+    </div> -->
+    <div class="login-page">
+      <div class="container">
+          <div class="section login-form">
+            <div class="d-flex justify-content-center">
+              <h2 class="title">Đăng nhập</h2>
+            </div>
+            <form method="POST" action="/register" id="dinhkemform" enctype="multipart/form-data">
+              <div class="login">
+                  <input type="text" class="form-control" placeholder="(*) Địa chỉ email">
+                  <input type="text" class="form-control" placeholder="(*) Mật khẩu">
+                  <div class="col-md-12 text-center">
+                    <button class="btn btn-primary">Đăng nhập</button>
+                  </div>
+              </div>
+            </form>
+          </div>
+      </div>
+    </div>
+    <div class="highlight">
+      <div class="container">
+        <div class="heading">
+          <h2>Highlight</h2>
+        </div>
+        <div class="row">
+          <div class="col-lg-3">
+            <div class="item">
+              <div class="box">
+                <a href="#" class="thumbnail">
+                  <img src="images/ex/news/1.jpg" alt="">
+                </a>
+                <p class="text">Lorem ipsum dolor amet, consectetur adipiscing elit. Cras ac velit ex. Mauris mollis varius lacus ultrices </p>
+                <p class="text-center"><a href="#" class="btn btn-primary">Read more</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3">
+            <div class="item">
+              <div class="box">
+                <a href="#" class="thumbnail">
+                  <img src="images/ex/news/2.jpg" alt="">
+                </a>
+                <p class="text">Lorem ipsum dolor amet, consectetur adipiscing elit. Cras ac velit ex. Mauris mollis varius lacus ultrices </p>
+                <p class="text-center"><a href="#" class="btn btn-primary">Read more</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3">
+            <div class="item">
+              <div class="box">
+                <a href="#" class="thumbnail">
+                  <img src="images/ex/news/3.jpg" alt="">
+                </a>
+                <p class="text">Lorem ipsum dolor amet, consectetur adipiscing elit. Cras ac velit ex. Mauris mollis varius lacus ultrices </p>
+                <p class="text-center"><a href="#" class="btn btn-primary">Read more</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3">
+            <div class="item">
+              <div class="box">
+                <a href="#" class="thumbnail">
+                  <img src="images/ex/news/1.jpg" alt="">
+                </a>
+                <p class="text">Lorem ipsum dolor amet, consectetur adipiscing elit. Cras ac velit ex. Mauris mollis varius lacus ultrices </p>
+                <p class="text-center"><a href="#" class="btn btn-primary">Read more</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+<?php include('footer.php') ?>
+</body>
+</html>
